a7bbb311e1a56be6f88127e4a3cdbaed221fc8f8
alxalvs <alxalvs0121@gmail.com>
2018-03-16 16:47:18 -0300

    json communication to php code added

--- ecamaraPhonegap/php/validarAcesso.php
+++ ecamaraPhonegap/php/validarAcesso.php
@@ -2,48 +2,32 @@
 
 	require 'include/connection.php';
 
-	$email = $_POST['email'];
-	$senha = $_POST['senha'];
+	$email = $_POST["formEntrarEmail"];
+	$senha = $_POST['formEntrarSenha'];
 
 
 	$sql = "SELECT * FROM usuariosnew WHERE email ='$email' AND senha = '$senha' ";
 
 	$connection = mysqli_connect($server, $user, $password, $database);
-
-             
+    
+    $arr = array();       
     $result = mysqli_query($connection, $sql);
 
     if ($result) {
-        $dadosUsuario = mysqli_fetch_array($result);
+        $dadosUsuario = mysqli_fetch_object($result);
 
-        if (isset($dadosUsuario['email'])) {
-            $name = $dadosUsuario['name'];
-            $emailUser = $dadosUsuario['email'];
-        	
-        }else{
-        	$name = null;
-            $emailUser = null;
-        }
+        
+            $arr['result'] = true;
+        	$arr['name'] = $dadosUsuario->name;
+            $arr['email'] = $dadosUsuario->email;
+            
     }else{
-    	echo "erro na execução da consulta, favor contate o administrador do app.";
+        	$arr['result'] = false;
+            
     }
-                  
+  
    mysqli_close($connection);
+   echo json_encode($arr);
 
 ?>
-<script>
 
-var emailPhp = "<?php echo $emailUser;?>";
-var usuarioPhp = "<?php echo $name;?>";
-// Cria um item "usuario" com valor da variavel "$name"
-window.localStorage.setItem('usuario', usuarioPhp );
-window.localStorage.setItem('emailUsuario', emailPhp);
-
-if (window.localStorage.getItem('usuario')!= null){
-    window.location = "http://localhost/ecamaraProjeto/ecamara/ecamaraphonegap/www/index.html";
-}else{
-    alert('não foi possivel armazenar valores localmente.');
-    window.location = "http://localhost/ecamaraProjeto/ecamara/ecamaraphonegap/www/index.html";
-}
-
-</script>
